77ba8a0e675badbef9004bdb0d8c16d937c75031
flinux-sudo <filipgondek78@gmail.com>
2020-11-03 20:00:49 +0100

    i think it works ok it's around fifty-fifty most of the time the number of heads and tailes

--- src/com/newsoperators/CoinFlipping.java
+++ src/com/newsoperators/CoinFlipping.java
@@ -1,7 +1,7 @@
 package com.newsoperators;
 
 import java.util.Random;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class CoinFlipping {
     String coinsides="";
@@ -9,23 +9,36 @@ public class CoinFlipping {
     public static void main(String[] args) {
         CoinFlipping coin=new CoinFlipping();
         Random rands =new Random();
-        Scanner scaner=new Scanner(System.in);
+     //   Scanner scaner=new Scanner(System.in);
 
         System.out.println("flipcoin:");
         System.out.println("to continue game input some sign" +
                 "to end game input :E=END");
+        int limit=1000;
+        int iterator=0;
+        int numberofheads=0;
+        int numberoftailes=0;
         do {
             int sides = rands.nextInt(2);
 
             if (sides == 1) {
                 coin.coinsides = "Heads";
+                numberofheads++;
             } else {
                 coin.coinsides = "Tailes";
+                numberoftailes++;
             }
             System.out.println(coin.coinsides);
             System.out.println(sides);
             System.out.println("continue:");
-        }while(!(scaner.next().equals("E")));
+            System.out.println("iterator: "+iterator++);
+        }while(iterator<limit);
+        System.out.println("numberofheads: "+numberofheads);
+        System.out.println("numberoftailes: "+numberoftailes);
+
+
+
+
     }
 
 }
